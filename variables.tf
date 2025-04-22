7565344966161142b6bda98d8c7273945342d950
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 15:18:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,24 @@
+variable "key_name" {
+  description = "Key pair name to use for instances"
+  type        = string
+}
 
+variable "subnet_map" {
+  description = "Map of instance names to subnet IDs"
+  type        = map(string)
+}
+
+variable "sg_map" {
+  description = "Map of instance names to security group IDs"
+  type        = map(list(string))
+}
+
+variable "ec2_instances" {
+  description = "Map of EC2 instance definitions"
+  type = map(object({
+    ami           = string
+    instance_type = string
+    root_volume   = number
+    ebs_volume    = optional(number)
+  }))
+}
