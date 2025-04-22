ed2f56451f95ab8ba73c7f0b906059af1381e5ae
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 17:15:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,21 @@ provider "aws" {
 module "vpc" {
   source = "./modules/vpc"
 
-  vpc_cidr                 = "10.0.0.0/16"
-  vpc_name                 = "gcc-vpc-facial-vpc"
-  public_subnet_1_cidr     = "10.0.0.0/20"
-  public_subnet_1_name     = "gcc-vpc-facial-subnet-public1-ap-south-1a"
-  public_subnet_2_cidr     = "10.0.16.0/20"
-  public_subnet_2_name     = "gcc-vpc-facial-subnet-public2-ap-south-1b"
-  private_subnet_1_cidr    = "10.0.128.0/20"
-  private_subnet_1_name    = "gcc-vpc-facial-subnet-private1-ap-south-1a"
-  private_subnet_2_cidr    = "10.0.144.0/20"
-  private_subnet_2_name    = "gcc-vpc-facial-subnet-private2-ap-south-1b"
-  public_route_table_name  = "gcc-vpc-facial-rtb-public"
-  private_route_table_1_name = "gcc-vpc-facial-rtb-private1-ap-south-1a"
-  private_route_table_2_name = "gcc-vpc-facial-rtb-private2-ap-south-1b"
-  igw_name                 = "gcc-vpc-facial-igw"
-  nat_gateway_name         = "gcc-vpc-facial-nat-public1-ap-south-1a"
+  vpc_cidr                  = var.vpc_cidr
+  vpc_name                  = var.vpc_name
+  public_subnet_1_cidr      = var.public_subnet_1_cidr
+  public_subnet_1_name      = var.public_subnet_1_name
+  public_subnet_2_cidr      = var.public_subnet_2_cidr
+  public_subnet_2_name      = var.public_subnet_2_name
+  private_subnet_1_cidr     = var.private_subnet_1_cidr
+  private_subnet_1_name     = var.private_subnet_1_name
+  private_subnet_2_cidr     = var.private_subnet_2_cidr
+  private_subnet_2_name     = var.private_subnet_2_name
+  public_route_table_name   = var.public_route_table_name
+  private_route_table_1_name = var.private_route_table_1_name
+  private_route_table_2_name = var.private_route_table_2_name
+  igw_name                  = var.igw_name
+  nat_gateway_name          = var.nat_gateway_name
 }
 module "ec2_instances" {
   source = "./modules/ec2"
