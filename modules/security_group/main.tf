523c066bf0dcc5cf5001a546647f912f27b89a3b
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 15:39:35 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,1 +1,24 @@
 
+resource "aws_security_group" "this" {
+  for_each = var.security_groups
+
+  name        = each.key
+  description = each.value.description
+  vpc_id      = each.value.vpc_id
+
+  dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      description      = lookup(ingress.value, "description", null)
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
+      protocol         = ingress.value.protocol
+      cidr_blocks      = lookup(ingress.value, "cidr_blocks", null)
+      security_groups  = lookup(ingress.value, "security_groups", null)
+    }
+  }
+
+  tags = {
+    Name = each.key
+  }
+}
