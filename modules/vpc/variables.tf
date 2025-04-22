02c075a8d984c98d5847ece81750a52019d8ae1e
ganeshkumar-cpu <ganeshkumar.u@ilios.digital>
2025-04-22 17:19:35 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,90 +1,59 @@
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
-  description = "The name of the VPC"
-  type        = string
-  default     = "gcc-vpc-facial-vpc"
+  description = "The name tag to assign to the VPC"
 }
 
 variable "public_subnet_1_cidr" {
-  description = "CIDR block for the public subnet 1"
-  type        = string
-  default     = "10.0.0.0/20"
+  description = "CIDR block for the first public subnet (e.g., in ap-south-1a)"
 }
 
 variable "public_subnet_1_name" {
-  description = "The name for public subnet 1"
-  type        = string
-  default     = "gcc-vpc-facial-subnet-public1-ap-south-1a"
+  description = "Name tag for the first public subnet"
 }
 
 variable "public_subnet_2_cidr" {
-  description = "CIDR block for the public subnet 2"
-  type        = string
-  default     = "10.0.16.0/20"
+  description = "CIDR block for the second public subnet (e.g., in ap-south-1b)"
 }
 
 variable "public_subnet_2_name" {
-  description = "The name for public subnet 2"
-  type        = string
-  default     = "gcc-vpc-facial-subnet-public2-ap-south-1b"
+  description = "Name tag for the second public subnet"
 }
 
 variable "private_subnet_1_cidr" {
-  description = "CIDR block for the private subnet 1"
-  type        = string
-  default     = "10.0.128.0/20"
+  description = "CIDR block for the first private subnet (e.g., in ap-south-1a)"
 }
 
 variable "private_subnet_1_name" {
-  description = "The name for private subnet 1"
-  type        = string
-  default     = "gcc-vpc-facial-subnet-private1-ap-south-1a"
+  description = "Name tag for the first private subnet"
 }
 
 variable "private_subnet_2_cidr" {
-  description = "CIDR block for the private subnet 2"
-  type        = string
-  default     = "10.0.144.0/20"
+  description = "CIDR block for the second private subnet (e.g., in ap-south-1b)"
 }
 
 variable "private_subnet_2_name" {
-  description = "The name for private subnet 2"
-  type        = string
-  default     = "gcc-vpc-facial-subnet-private2-ap-south-1b"
+  description = "Name tag for the second private subnet"
 }
 
 variable "public_route_table_name" {
-  description = "The name for the public route table"
-  type        = string
-  default     = "gcc-vpc-facial-rtb-public"
+  description = "Name tag for the public route table"
 }
 
 variable "private_route_table_1_name" {
-  description = "The name for private route table 1"
-  type        = string
-  default     = "gcc-vpc-facial-rtb-private1-ap-south-1a"
+  description = "Name tag for the private route table associated with the first private subnet"
 }
 
 variable "private_route_table_2_name" {
-  description = "The name for private route table 2"
-  type        = string
-  default     = "gcc-vpc-facial-rtb-private2-ap-south-1b"
+  description = "Name tag for the private route table associated with the second private subnet"
 }
 
 variable "igw_name" {
-  description = "The name for the internet gateway"
-  type        = string
-  default     = "gcc-vpc-facial-igw"
+  description = "Name tag for the Internet Gateway"
 }
 
 variable "nat_gateway_name" {
-  description = "The name for the NAT Gateway"
-  type        = string
-  default     = "gcc-vpc-facial-nat-public1-ap-south-1a"
+  description = "Name tag for the NAT Gateway"
 }
-
